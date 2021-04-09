452ff94e3d788d7fdbe7e9117610bccaff91e85c
Jesse Wenger <wengerj@gmail.com>
2021-04-09 02:01:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +9,51 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  post '/articles' do
+    article = Article.create(params[:article])
+    article.save
+    redirect to "articles/#{article.id}"
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.update(title: params[:article][:title], content: params[:article][:content])
+    redirect to "articles/#{article.id}"
+  end
+  
+  delete '/articles/:id' do
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
